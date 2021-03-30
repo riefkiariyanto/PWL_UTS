5d13d22df1550d66b6d591f3a3df35f676688366
= <=>
2021-03-30 15:09:12 +0700

    Mengedit isi dari seeder

--- database/seeders/BarangSeeder.php
+++ database/seeders/BarangSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use DB;
 
 class BarangSeeder extends Seeder
 {
@@ -13,6 +14,16 @@ class BarangSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $data = array(
+            [
+                'id' => 1,
+                'kode_barang' => 'c1000',
+                'nama_barang' => 'Marjan',
+                'kategori_barang' =>'Minuman',
+                'harga' =>"24000",
+                'qty' =>"79",
+            ],
+        );
+        DB::table('Barangs')->insert($data);
     }
 }
