68c086c8732e3d31ef342fd3aa7bf09c146c9ff9
= <=>
2021-03-31 04:35:10 +0700

    menambahkan route cari barang

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BarangController;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,3 +16,4 @@ use App\Http\Controllers\BarangController;
 */
 
 Route::resource('Barang', BarangController::class);
+Route::get('Barang/cari/',[BarangCOntroller::class, 'search']);
